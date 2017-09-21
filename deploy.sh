f552b9ff3f5cb8e9905344e66263ceb799cb3821
XIONG Chui <hufan.coder@gmail.com>
2017-09-21 22:04:33 +0800

    使用 supervisor 配置并监控app

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,16 @@ app_name='blog-sanic'
 
 # 保持 nginx 配置文件和项目名称相同
 nginx_file="$app_name.nginx"
+supervisor_conf="$app_name.conf"
 log_path="/tmp/$app_name.log"
 
 echo "deploy $app_name app starting"
 mv /etc/nginx/sites-enabled/* /tmp
 rm /tmp/* -rf
 ln -s "/var/www/$app_name/$nginx_file" "/etc/nginx/sites-enabled/$app_name"
+ln -s "/var/www/$app_name/$supervisor_conf" "/etc/supervisor/conf.d/$supervisor_conf"
 cd "/var/www/$app_name"
-gunicorn wsgi:application -c conf-gunicorn.py > $log_path 2>&1 &
+gunicorn wsgi:application -c conf-gunicorn.py &
 service nginx restart
+service supervisor restart
 echo "deploy $app_name app succeeded"
--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo 'install requirements starting'
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install python3 python3-dev python3-pip python3-setuptools mongodb build-essential nginx -y
+sudo apt-get install python3 python3-dev python3-pip python3-setuptools mongodb build-essential nginx supervisor -y
 pip3 install gunicorn
 pip3 install -r requirements.txt
 
