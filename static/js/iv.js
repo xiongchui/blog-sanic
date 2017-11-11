3d6cfd72ce6afe8447ef5d7d86da7b33c410434c
XIONG Chui <xiongchui.coder@gmail.com>
2017-11-11 14:42:26 +0800

    user 登录注册逻辑使用 类 重构

--- static/js/iv.js
+++ static/js/iv.js
@@ -32,11 +32,11 @@ Element.prototype.on = Element.prototype.addEventListener
 
 
 // 封装绑定事件函数
-var bindEvent = (element, eventName, callback) => {
+const bindEvent = (element, eventName, callback) => {
     element.addEventListener(eventName, callback)
 }
 
-var bindEventDelegate = (element, eventName, callback, responseClass) => {
+const bindEventDelegate = (element, eventName, callback, responseClass) => {
     element.addEventListener(eventName, event => {
         var self = event.target
         if (self.classList.contains(responseClass)) {
@@ -45,7 +45,7 @@ var bindEventDelegate = (element, eventName, callback, responseClass) => {
     })
 }
 
-var bindAll = (selector, eventName, callback, responseClass) => {
+const bindAll = (selector, eventName, callback, responseClass) => {
     var es = document.querySelectorAll(selector)
     var func = responseClass === undefined ? bindEvent : bindEventDelegate
     for (let e of es) {
@@ -119,4 +119,113 @@ api.post = function (url, form, callback) {
         data: JSON.stringify(form),
     }
     api.ajax(r).then(callback, alert)
+}
+
+class Api {
+    static single() {
+        if (this._instance === undefined) {
+            this._instance = new this()
+        }
+        return this._instance
+    }
+
+    _ajax(request) {
+        var req = {
+            url: request.url,
+            data: JSON.stringify(request.data) || null,
+            method: request.method || 'POST',
+            header: request.header || {},
+            contentType: request.contentType || 'application/json',
+            callback: request.callback
+        }
+        var r = new XMLHttpRequest()
+        var promise = new Promise((resolve, reject) => {
+            r.open(req.method, req.url, true)
+            r.setRequestHeader('Content-Type', req.contentType)
+            // setHeader
+            if (request.XCSRFToken !== undefined) {
+                r.setRequestHeader('X-CSRFToken', request.XCSRFToken)
+            }
+            Object.keys(req.header).forEach(key => {
+                r.setRequestHeader(key, req.header[key])
+            })
+            r.onreadystatechange = function () {
+                if (r.readyState === 4) {
+                    let res = r.response
+                    // 回调函数
+                    if (typeof req.callback === 'function') {
+                        req.callback(res)
+                    }
+                    // Promise 成功
+                    resolve(res)
+                }
+            }
+            r.onerror = function (err) {
+                reject(err)
+            }
+            if (req.method.toUpperCase() === 'GET') {
+                r.send()
+            } else {
+                r.send(req.data)
+            }
+        })
+        return promise
+    }
+
+    get(path, callback) {
+        const req = {
+            url: path,
+            method: 'GET',
+            callback: callback,
+        }
+        return this._ajax(req)
+    }
+
+    post(path, form, callback) {
+        const req = {
+            url: path,
+            data: form,
+            callback: callback,
+        }
+        return this._ajax(req)
+    }
+}
+
+class PubSub {
+    constructor() {
+        this.handlers = {}
+    }
+
+    on(eventType, handler) {
+        if (!(eventType in this.handlers)) {
+            this.handlers[eventType] = []
+        }
+        this.handlers[eventType].push(handler)
+        return this
+    }
+
+    emit(...args) {
+        const hs = this.handlers
+        const [eventType, ...rest] = args
+        for (let callback of hs[eventType]) {
+            callback.apply(this, rest)
+        }
+        return this
+    }
+}
+
+class Component extends PubSub {
+    constructor(props) {
+        super()
+        this._props = props
+        this.model = props.model
+        this.api = props.api
+        this.wrapper = props.wrapper
+        this.setup()
+    }
+
+    setup() {
+
+    }
+
 }
--- static/user/js/index.js
+++ static/user/js/index.js
@@ -1,73 +1,128 @@
-api.login = form => {
-    var req = {
-        url: '/users/login',
-        data: form,
+class ApiUser extends Api {
+    constructor() {
+        super()
+        this.path = '/users'
     }
-    api.ajax(req).then(body => {
-        var r = JSON.parse(body)
+
+    login(form, callback) {
+        const url = this.path + '/login'
+        return this.post(url, form, callback)
+    }
+
+    register(form, callback) {
+        const url = this.path + '/register'
+        return this.post(url, form, callback)
+    }
+}
+
+class AppUser extends Component {
+    constructor(props) {
+        super(props)
+        this.render()
+        this.bindEventClick()
+    }
+
+    render() {
+        const w = this.wrapper
+        w.innerHTML = this.template()
+    }
+
+    template() {
+        const s = `
+        <div class="user-container">
+            <label for="id-register-username">
+                用户名
+                <input id="id-register-username" class="user-info" type="text" name="username">
+            </label>
+            <label for="id-register-email">
+                邮箱
+                <input class="user-info" type="email" name="email">
+            </label>
+            <label for="id-register-password">
+                密码
+                <input id="id-register-password" class="user-info" type="password" name="password">
+            </label>
+            <button type="submit" data-action="user-register">register</button>
+        </div>
+        <div class="user-container">
+            <label id="id-register-email" for="id-login-username">
+                用户名
+                <input class="user-info" type="text" name="username">
+            </label>
+            <label for="id-login-username">
+                密码
+                <input class="user-info" type="password" name="password">
+            </label>
+            <button type="submit" data-action="user-login">login</button>
+        </div>`
+        return s
+    }
+
+    bindEventClick() {
+        const mapAction = {
+            'user-login': this.actionLogin,
+            'user-register': this.actionRegister,
+        }
+        this.wrapper.on('click', (e) => {
+            const self = e.target
+            const action = self.dataset.action
+            log('action', action)
+            const fn = mapAction[action]
+            if (fn !== undefined) {
+                fn.call(this, e)
+            }
+        })
+    }
+
+    formFromEvent(event) {
+        const self = event.target
+        const container = self.closest('.user-container')
+        const inputs = container._es('.user-info')
+        const form = {}
+        inputs.forEach(e => {
+            var key = e.name
+            form[key] = e.value
+        })
+        log('form', form)
+        return form
+    }
+
+    actionLogin(event) {
+        const form = this.formFromEvent(event)
+        const p = this.api.login(form)
+        p.then(res => {
+            this.callback(res)
+        })
+    }
+
+    actionRegister(event) {
+        const form = this.formFromEvent(event)
+        const p = this.api.register(form)
+        p.then(res => {
+            this.callback(res)
+        })
+    }
+
+    callback(response) {
+        var r = JSON.parse(response)
         if (r.success) {
             location.href = `/users/detail`
         } else {
             alert(`${r.msgs.join('')}`)
         }
-    })
-}
-
-api.register = form => {
-    var req = {
-        url: '/users/register',
-        data: form,
     }
-    api.ajax(req).then(body => {
-        var r = JSON.parse(body)
-        if (r.success) {
-            location.href = `/users/detail`
-        } else {
-            alert(`${r.msgs.join('')}`)
-        }
-    })
 }
 
-var formFromEvent = event => {
-    var self = event.target
-    var container = self.closest('.user-container')
-    var inputs = container._es('.input-info')
-    var form = {}
-    inputs.forEach(e => {
-        var key = e.name
-        form[key] = e.value
-    })
-    return form
-}
-
-var actionLogin = event => {
-    var form = formFromEvent(event)
-    api.login(form)
-}
-
-var actionRegister = event => {
-    var form = formFromEvent(event)
-    api.register(form)
-}
-
-const bindEventsClick = () => {
-    var actions = {
-        login: actionLogin,
-        register: actionRegister,
+const initApp = () => {
+    const props = {
+        wrapper: _e('#id-app-user'),
+        api: ApiUser.single(),
     }
-    var container = _e('#id-action-container')
-    container.addEventListener('click', event => {
-        var self = event.target
-        var actionName = self.dataset.action
-        var action = actions[actionName]
-        if (action !== undefined) {
-            action(event)
-        }
-    })
+    new AppUser(props)
 }
 
 const __main = () => {
-    bindEventsClick()
+    initApp()
 }
 
 __main()
