27e4f9f24d45fa60d8abf9a15371100226c4637c
XIONG Chui <xiongchui.coder@gmail.com>
2017-11-13 19:40:42 +0800

    管理页面初步完成

--- static/article/js/manage-detail.js
+++ static/article/js/manage-detail.js
@@ -1,127 +1,290 @@
-var changeTitle = (article) => {
-    var title = '半夜集 - ' + article.title
-    document.title = title
+class NunjucksEnv {
+    constructor() {
+        const env = new nunjucks.Environment()
+        env.addFilter('formattime', time => {
+            var d = new Date(time * 1000)
+            var year = d.getFullYear()
+            var month = d.getMonth() + 1
+            var date = d.getDate()
+            return `${month}/${date}/${year}`
+        })
+        return env
+    }
+
+    static single() {
+        if (this._instance === undefined) {
+            this._instance = new this()
+        }
+        return this._instance
+    }
 }
 
-var htmlFromMarkdown = (markdown) => {
-    var s = md.render(markdown)
-    return s
+class ApiArticle extends Api {
+    constructor() {
+        super()
+        this.path = '/api'
+    }
+
+    all(callback) {
+        const url = this.path + '/articles'
+        return this.get(url, callback)
+    }
+
+    allSync() {
+        const url = this.path + '/articles'
+        return this.getSync(url)
+    }
+
+    addComment(form, callback) {
+        const url = this.path + '/comment/add'
+        return this.post(url, form, callback)
+    }
+
+    deleteArticle(articleId, callback) {
+        const url = this.path + `/articles/delete/${articleId}`
+        return this.get(url, callback)
+    }
 }
 
-var templateArticle = (article) => {
-    var m = article
-    var s = `<header>
-    <h1 id="id-article-title">
-        {{ m.title }}
-    </h1>
-    <div class="detail-info flex">
-    <span id="id-article-ct">
-        <i class="fa fa-calendar-check-o fa-fw" aria-hidden="true"></i>
-        发表于
-        <a href="#delete/{{ m.id }}">{{ m.ct | formattime }}</a>
-    </span>
-        <span id="id-article-ut">
-        <i class="fa fa-calendar fa-fw" aria-hidden="true"></i>
-        更新于
+class AppBlog {
+    constructor() {
+        this.href = location.href
+        this.history = window.history
+        this.render()
+        this.bindEvent()
+    }
+
+    render() {
+        const hash = location.hash
+        const s = hash === '' ? 'category/all' : hash.slice(1)
+        this.changeView(s)
+    }
+
+    bindEvent() {
+        window.addEventListener('hashchange', (e) => {
+            const [url, hash] = e.newURL.split('#')
+            const flag = url.endsWith('/manage')
+            if (flag) {
+                log('hash', hash)
+                this.changeView(hash)
+            }
+        })
+    }
+
+    changeView(hash) {
+        var [view, _] = hash !== undefined ? hash.split('/') : ['category', undefined]
+        var cls = {
+            'category': ViewArticle,
+            'detail': ViewDetail,
+        }
+        if (this.view !== undefined) {
+            this.view.destroy()
+        }
+        this.view = this.initView(cls[view], hash)
+        log('view', this.view)
+    }
+
+    initView(cls, hash) {
+        const props = {
+            model: Article.single(),
+            api: ApiArticle.single(),
+            wrapper: _e('#id-app-article')
+        }
+        return new cls(props, hash)
+    }
+}
+
+class ViewArticle extends Component {
+    constructor(props, hash) {
+        super(props)
+        this.hash = hash
+        this.nunjucks = NunjucksEnv.single()
+        this.initEnv()
+        this.categories = ['javascript', 'python', 'mind']
+        this.renderByCategory()
+    }
+
+    initEnv() {
+        const s = `
+    <div id="id-article-container" class="fadeIn slideDown">
+        <article>
+        </article>
+        <div id="id-comment-container">
+        </div>
+    </div>`
+        this.wrapper.innerHTML = s
+    }
+
+    category() {
+        const [_, sub] = this.hash.split('/')
+        return sub
+    }
+
+    _renderCells(records) {
+        let arr = records
+        if (this.categories.includes(this.category())) {
+            arr = arr.filter(e => e.category === this.category())
+            alertify.success(`load category ${this.category()} successfully`)
+        }
+        const env = this.nunjucks
+        const template = this.templateCell()
+        const key = 't'
+        const cells = arr.map(m => {
+            const args = {}
+            args[key] = m
+            const s = env.renderString(template, args)
+            return s
+        })
+        const div = this.wrapper._e('#id-article-container')
+        div.innerHTML = cells.join('\n')
+    }
+
+    renderByCategory() {
+        let arr = this.model.all()
+        this._renderCells(arr)
+    }
+
+    templateCell() {
+        const s = `<article id="id-article-{{ t.id }}" class="article-cell">
+    <div class="article-title">
+        <a class="article-link" href="#detail/{{ t.id }}">{{ t.title }}</a>
+    </div>
+    <div class="article-info">
+        <span><i class="fa fa-calendar-check-o fa-fw" aria-hidden="true"></i>{{ t.ct | formattime }}</span>
+        <span><i class="fa fa-calendar fa-fw" aria-hidden="true"></i>{{ t.ut | formattime }}</span>
+        <span><i class="fa fa-tags fa-fw" aria-hidden="true"></i><a href="#category/{{ t.category }}">{{ t.category }}</a></span>
+    </div>
+    <div class="article-overview">{{ t.overview }}</div>
+    <div class="article-detail"><a href="#detail/{{ t.id }}">阅读全文</a></div>
+</article>`
+        return s
+    }
+}
+
+class ViewDetail extends Component {
+    constructor(props, hash) {
+        super(props)
+        this.hash = hash
+        this.id = this.idByHash(hash)
+        this.nunjucks = NunjucksEnv.single()
+        this.initEnv()
+        this.render()
+        this.bindEvents()
+    }
+
+    idByHash(hash) {
+        const [_, id] = hash.split('/')
+        return parseInt(id)
+    }
+
+    initEnv() {
+        const s = `<div id="id-article-container" class="fadeIn slideDown">
+        <article>
+        </article>
+            <div id="id-comment-container">
+            </div>
+        </div>`
+        this.wrapper.innerHTML = s
+    }
+
+    insertCommentInput(articleId) {
+        const s = `<div>
+            <input id="id-input-content" data-id="${articleId}" type="text" name="content">
+            <button data-action="add-comment" type="submit">add comment</button>
+        </div>
+        <div id="id-comment-content">
+        </div>`
+        const div = this.wrapper._e('#id-comment-container')
+        div.insertAdjacentHTML('afterbegin', s)
+    }
+
+    render() {
+        const arr = this.model.all()
+        this.renderArticle(arr)
+    }
+
+    renderArticle(articles) {
+        const ms = articles
+        const m = ms.find(e => e.id === this.id)
+        if (m !== undefined) {
+            this.changeTitle(m)
+            this.insertArticle(m)
+            this.insertCommentInput(this.id)
+            this.renderComment(m)
+            alertify.success(`load current article succeeded`)
+        } else {
+            alertify.error('没有这篇文章 跳转至上一页')
+            history.back()
+        }
+    }
+
+    renderComment(article) {
+        const ms = article.comments
+        var container = this.wrapper._e('#id-comment-content')
+        ms.forEach(m => {
+            var s = this.templateComment(m)
+            container.insertAdjacentHTML('beforeend', s)
+        })
+    }
+
+    changeTitle(model) {
+        const title = '半夜集 - ' + model.title
+        document.title = title
+    }
+
+    insertArticle(model) {
+        const m = model
+        m.content = this.htmlFromMarkdown(m.content)
+        const s = this.template(m)
+        const div = this.wrapper._e('article')
+        div.insertAdjacentHTML('afterbegin', s)
+    }
+
+    htmlFromMarkdown(markdown) {
+        const s = md.render(markdown)
+        return s
+    }
+
+    template(article) {
+        var m = article
+        var s = `<header>
+        <h1 id="id-article-title">
+            {{ m.title }}
+        </h1>
+        <div class="detail-info flex">
+        <span id="id-article-ct" data-id="{{ m.id }}">
+            <i class="fa fa-calendar-check-o fa-fw" aria-hidden="true"></i>
+            发表于
+            <a data-action="delete-article">{{ m.ct | formattime }}</a>
+        </span>
+            <span id="id-article-ut">
+            <i class="fa fa-calendar fa-fw" aria-hidden="true"></i>
+            更新于
             <a href="#edit/{{ m.id }}">{{ m.ut | formattime }}</a>
-    </span>
-        <span>
-        <i class="fa fa-tags fa-fw" aria-hidden="true"></i>
-        分类
-        <a href="#category/{{ m.category }}">{{ m.category }}</a>
-    </span>
+        </span>
+            <span>
+            <i class="fa fa-tags fa-fw" aria-hidden="true"></i>
+            分类
+            <a href="#category/{{ m.category }}">{{ m.category }}</a>
+        </span>
+        </div>
+    </header>
+    <div class="article-overview">
+        {{ m.overview }}
     </div>
-</header>
-<div class="article-overview">
-    {{ m.overview }}
-</div>
-<div id="id-article-content">
-    {{ m.content | safe }}
-</div>
-        `
-    var env = nunjucksEnvironment()
-    var r = env.renderString(s, {
-        m,
-    })
-    return r
-}
-
-var insertCommentInput = (articleId) => {
-    var s = `<div>
-    <input id="id-input-content" data-id="${articleId}" type="text" name="content">
-    <button id="id-btn-add" type="submit">add comment</button>
-</div>
-<div id="id-comment-content">
-</div>`
-    var container = _e('#id-comment-container')
-    container.insertAdjacentHTML('afterbegin', s)
-}
-
-var insertArticle = (article) => {
-    var r = article
-    r.content = htmlFromMarkdown(r.content)
-    var s = templateArticle(r)
-    var container = _e('article')
-    container.insertAdjacentHTML('afterbegin', s)
-}
-
-var loadComment = (r) => {
-    ms = r.comments
-    var container = _e('#id-comment-content')
-    ms.forEach(m => {
-        var s = templateComment(m)
-        container.insertAdjacentHTML('beforeend', s)
-    })
-}
-
-var loadArticleById = (id) => {
-    var container = _e('#id-article-container')
-    id = Number(id)
-    var body = localStorage.articles
-    var arr = JSON.parse(body)
-    var article = arr.filter(e => e.id === id)[0]
-    if (article !== undefined) {
-        var r = article
-        changeTitle(r)
-        insertArticle(r)
-        insertCommentInput(id)
-        loadComment(r)
-        bindEventAddComment()
-        alertify.success(`load current article succeeded`)
-    } else {
-        alertify.error('没有这篇文章 跳转至上一页')
-        history.back()
+    <div id="id-article-content">
+        {{ m.content | safe }}
+    </div>`
+        var env = this.nunjucks
+        var r = env.renderString(s, {
+            m,
+        })
+        return r
     }
-}
 
-var templateContainerArticle = () => {
-    var s = `<div id="id-article-container" class="fadeIn slideDown">
-    <article>
-    </article>
-    <div id="id-comment-container">
-    </div>
-</div>`
-    return s
-}
-
-api.addComment = (form, callback) => {
-    var req = {
-        data: form,
-        url: '/api/comment/add'
-    }
-    api.ajax(req).then(body => {
-        callback(body)
-    })
-}
-
-var insertComment = (article) => {
-    var s = templateComment(article)
-    var container = _e('#id-comment-content')
-    container.insertAdjacentHTML('beforeend', s)
-}
-
-var templateComment = (m) => {
-    var s = `<div>
+    templateComment(article) {
+        const m = article
+        const s = `<div>
         {{ m.user.username }}
     </div>
     <div>
@@ -130,66 +293,88 @@ var templateComment = (m) => {
     <div>
         {{ m.content }}
     </div>`
-    var env = nunjucksEnvironment()
-    var r = env.renderString(s, {
-        m,
-    })
-    return r
-}
+        var env = NunjucksEnv.single()
+        var r = env.renderString(s, {
+            m,
+        })
+        return r
+    }
 
-var bindEventAddComment = () => {
-    var btn = _e('#id-btn-add')
-    btn.addEventListener('click', event => {
-        var input = _e('#id-input-content')
-        var form = {
+    bindEvents() {
+        this.bindEventClick()
+    }
+
+    bindEventClick() {
+        const mapAction = {
+            'add-comment': this.actionAddComment,
+            'delete-article': this.actionDeleteArticle,
+        }
+        this.wrapper.on('click', (e) => {
+            const self = e.target
+            const action = self.dataset.action
+            log('action', action)
+            const fn = mapAction[action]
+            if (fn !== undefined) {
+                fn.call(this, e)
+            }
+        })
+    }
+
+    actionDeleteArticle(event) {
+        const self = event.target
+        const id = self.dataset.id
+        const p = this.api.deleteArticle(id)
+        p.then(body => {
+            var r = JSON.parse(body)
+            if (r.success) {
+                alertify.success('this article has been deleted successfully')
+                location.href = '/manage'
+            } else {
+                alertify.error(r.msgs.join(''))
+            }
+        })
+    }
+
+    actionAddComment(event) {
+        const self = event.target
+        const input = self.parentElement._e('#id-input-content')
+        const form = {
             content: input.value,
             article_id: input.dataset.id,
         }
-        api.addComment(form, body => {
+        const p = this.api.addComment(form)
+        p.then(body => {
             var r = JSON.parse(body)
             if (r.success) {
                 var data = r.data
-                insertComment(data)
+                this.insertComment(data)
                 alertify.success('add comment succeeded')
             } else {
                 alertify.error(r.msgs.join(''))
             }
         })
-    })
-}
-
-var initEnvDetail = () => {
-    var spa = _e('#id-spa')
-    var s = templateContainerArticle()
-    spa.insertAdjacentHTML('beforeend', s)
-}
-
-var initArticle = (hash) => {
-    initSpa()
-    initEnvDetail()
-    loadArticleById(hash)
-}
-
-var initSpa = () => {
-    var container = _e('#id-spa')
-    container.innerHTML = ''
-}
-
-var deleteArticle = (id) => {
-    var req = {
-        method: 'get',
-        url: `/api/articles/delete/${id}`
     }
-    api.ajax(req).then(body => {
-        var r = JSON.parse(body)
-        if (r.success) {
-            alertify.success('this article has been deleted successfully')
-            loadArticles()
-            location.href = '#category/all'
-        } else {
-            alertify.error(r.msgs.join(''))
+
+    insertComment(article) {
+        const s = this.templateComment(article)
+        const div = this.wrapper._e('#id-comment-content')
+        div.insertAdjacentHTML('beforeend', s)
+    }
+}
+
+class Article extends Model {
+    constructor() {
+        super()
+        this.api = ApiArticle.single()
+    }
+
+    all() {
+        if (this.records === undefined) {
+            const body = this.api.allSync()
+            this.records = JSON.parse(body).data
         }
-    })
+        return this.records
+    }
 }
 
 var insertArticleUpdateInput = (id) => {
@@ -288,7 +473,6 @@ const bindEventShow = () => {
     })
 }
 
-
 var updateArticle = (subHash) => {
     var id = Number(subHash)
     initSpa()
@@ -296,132 +480,26 @@ var updateArticle = (subHash) => {
     bindEventsUpdate(id)
 }
 
-
 var changeArticle = (hash) => {
     var [func, subHash] = hash !== undefined ? hash.split('/') : [undefined, undefined]
     var dic = {
-        'category': initArticles,
-        'detail': initArticle,
-        'delete': deleteArticle,
         'edit': updateArticle,
     }
     var f = dic[func] || initArticles
     f(subHash)
 }
 
-var bindEventHashChange = () => {
-    window.addEventListener('hashchange', (e) => {
-        var [url, hash] = e.newURL.split('#')
-        var flag = url.endsWith('/manage')
-        if (flag) {
-            changeArticle(hash)
-        }
-    })
-}
-
-var templateCellArticle = () => {
-    var s = `<article id="id-article-{{ t.id }}" class="article-cell">
-    <div class="article-title">
-        <a class="article-link" href="#detail/{{ t.id }}">{{ t.title }}</a>
-    </div>
-    <div class="article-info">
-        <span><i class="fa fa-calendar-check-o fa-fw" aria-hidden="true"></i>{{ t.ct | formattime }}</span>
-        <span><i class="fa fa-calendar fa-fw" aria-hidden="true"></i>{{ t.ut | formattime }}</span>
-        <span><i class="fa fa-tags fa-fw" aria-hidden="true"></i><a href="#category/{{ t.category }}">{{ t.category }}</a></span>
-    </div>
-    <div class="article-overview">{{ t.overview }}</div>
-    <div class="article-detail"><a href="#detail/{{ t.id }}">阅读全文</a></div>
-</article>`
-    return s
-}
-
-var nunjucksEnvironment = () => {
-    // setup nunjucks
-    var env = new nunjucks.Environment()
-    env.addFilter('formattime', time => {
-        var d = new Date(time * 1000)
-        var year = d.getFullYear()
-        var month = d.getMonth() + 1
-        var date = d.getDate()
-        return `${month}/${date}/${year}`
-    })
-    return env
-}
-
-// 自动获取数据并生成页面
-var loadArticles = () => {
-    const source = '/api/articles'
-    api.get(source, body => {
-        var res = JSON.parse(body)
-        if (res.success) {
-            var body = JSON.stringify(res.data)
-            localStorage.articles = body
-            var hash = location.hash.slice(1)
-            changeArticle(hash)
-            alertify.success('load all articles successfully')
-        } else {
-            alertify.error(msgs.join(''))
-        }
-    })
-}
-
-var loadArticlesByHash = (subHash) => {
-    var body = localStorage.articles
-    var arr = ['javascript', 'python', 'mind']
-    var d = JSON.parse(body)
-    if (arr.includes(subHash)) {
-        d = d.filter(e => e.category === subHash)
-        if (d.length > 0) {
-            alertify.success(`load category ${subHash} successfully`)
-        } else {
-            alertify.error(`${subHash} category has no articles`)
-        }
-    } else {
-        var exs = [undefined, 'all']
-        if (!exs.includes(subHash)) {
-            alertify.error(`no ${subHash} category and load all articles`)
-        }
+const initApp = () => {
+    const props = {
+        model: Article.single(),
+        api: ApiArticle.single(),
+        wrapper: _e('#id-app-article')
     }
-    insertArticles(d)
+    new AppBlog(props)
 }
 
-var insertArticles = (articles) => {
-    var env = nunjucksEnvironment()
-    var container = _e('#id-articles-container')
-    const template = templateCellArticle()
-    const key = 't'
-    let cells = []
-    articles.forEach(m => {
-        let args = {}
-        args[key] = m
-        let s = env.renderString(template, args)
-        cells.push(s)
-    })
-    container.innerHTML = cells.join('')
-}
-
-var templateContainerArticles = () => {
-    var s = `<div id="id-articles-container"
-     class="fadeIn slideDown">
-</div>`
-    return s
-}
-
-var initEnvArticles = () => {
-    var spa = _e('#id-spa')
-    var s = templateContainerArticles()
-    spa.insertAdjacentHTML('beforeend', s)
-}
-
-var initArticles = (hash) => {
-    initSpa()
-    initEnvArticles()
-    loadArticlesByHash(hash)
-}
-
-var __main = () => {
-    loadArticles()
-    bindEventHashChange()
+const __main = () => {
+    initApp()
 }
 
 __main()
