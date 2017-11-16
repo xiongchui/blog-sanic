90940f702d40be1ee67e958e0214cc30c3aa5da1
XIONG Chui <xiongchui.coder@gmail.com>
2017-11-16 14:33:05 +0800

    删除单独的add 功能

--- routes/article.py
+++ routes/article.py
@@ -9,12 +9,6 @@ async def index(request):
     return template('article/index.html')
 
 
-@bp.route('/manage/new', methods=['GET'])
-@login_required
-async def new(request):
-    return template('article/new.html')
-
-
 @bp.route('/manage')
 @login_required
 async def index(request):
